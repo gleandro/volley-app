ca80845b3ca48f4d26f5978929ce80bbfbd9d618
Giancarlo Jheynson Leandro Romo <giancarlosleandro1@gmail.com>
2025-03-07 20:16:29 -0500

    Parametrizar URL de la API para obtener jugadores y agregar archivos de entorno

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import Player from '../interfaces/player';
+import { environment } from '../enviroments/enviroment';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
-  private jsonUrl = 'player.json';
+  private apiUrl = `${environment.apiUrl}/players`; // URL parametrizada
 
   constructor(private http: HttpClient) {}
 
   getPlayers(): Observable<Player[]> {
-    return this.http.get<Player[]>(this.jsonUrl);
+    return this.http.get<Player[]>(this.apiUrl);
   }
 }
